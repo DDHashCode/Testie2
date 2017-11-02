ebf4d04e2ad1e86ae956409605c245f36fbc1ac9
DDHashCode <dembinski.dawid@gmail.com>
2017-11-02 15:47:56 +0100

    Ticket added

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,4 +1,14 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Ticket {
     public static int id = 0;
+    LocalDateTime timeCreated;
+    User ticketCreator;
+    User ticketOwner;
+    Queue assignedQueue;
+    ArrayList<String> log;
+    TicketPriority priority;
+
 
 }
